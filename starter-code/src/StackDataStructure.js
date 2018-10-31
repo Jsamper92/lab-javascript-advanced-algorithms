26352866c9037f37e48df26b0c9c50e90cb27248
Javier Samper <jsamperarias@hotmail.com>
2018-10-31 23:50:01 +0100

    Primera iteracion

--- starter-code/src/StackDataStructure.js
+++ starter-code/src/StackDataStructure.js
@@ -1,2 +1,33 @@
-function StackDataStructure () {
+function StackDataStructure() {
+    this.MAX_SIZE = 10;
+    this.stackControl = [];
+
+
+}
+
+StackDataStructure.prototype.isEmpty = function() {
+    return this.stackControl.length === 0;
+};
+
+StackDataStructure.prototype.canPush = function() {
+    return this.stackControl.length < this.MAX_SIZE;
+};
+
+StackDataStructure.prototype.push = function(num) {
+    if (this.canPush()) {
+        this.stackControl.push(num);
+        return this.stackControl;
+    } else {
+        return "Stack Overflow";
+    }
+}
+
+StackDataStructure.prototype.pop = function(num) {
+    if (!this.isEmpty()) {
+        ultimoElemento = this.stackControl[this.stackControl.length - 1]
+        this.stackControl.pop();
+        return ultimoElemento;
+    } else {
+        return "StackUnderflow";
+    }
 }
